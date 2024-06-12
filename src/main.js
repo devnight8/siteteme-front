ab59a767bcbef841ed8314467ea49855c1fccdf1
devnight <siteteme@gamil.com>
2024-06-12 12:09:08 +0330

    fix bug in form register

--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
 const formLogin = document.querySelector("#form-login");
 const btnLogin = document.querySelector("#btn-regiter");
 const closeBtn = document.querySelector("#close-btn");
-btnLogin.addEventListener("click", () => {
-  formLogin.classList.add("flex");
 
-  formLogin.classList.remove("hidden");
-});
+btnLogin.addEventListener("click",()=>{
+  formLogin.classList.toggle('invisible')
+})
 
-closeBtn.addEventListener("click", () => {
-  formLogin.classList.remove("flex");
-  formLogin.classList.add("hidden");
-});
+closeBtn.addEventListener("click",()=>{
+  formLogin.classList.toggle('invisible')
 
-
-// const listMenu = document.querySelector('.list-menu')
-// const Submenu = document.querySelector('.sub-menu')
-//
-// listMenu.addEventListener('click',(e)=>{
-//   e.preventDefault();
-//   Submenu.classList.toggle('hidden');
-//   // Submenu.classList.toggle('block');
-// })
-//
+})
